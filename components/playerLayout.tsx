d5e5f162247f69c0b1aa98d67b1441007bf1a68d
hellomaryjo <majorx3music@gmail.com>
2022-07-12 17:28:46 -0500

    add prisma, building seed script

---
+++ components/playerLayout.tsx
@@ -0,0 +1,20 @@
+import {Box} from  "@chakra-ui/layout";
+import Sidebar from "./sidebar";
+
+const PlayerLayout = ({ children }) => {
+  return (
+    <Box width="100vw" height="100vh">
+      <Box left="0" position="absolute" top="0" width="250px">
+        <Sidebar />
+      </Box>
+      <Box marginLeft="250px">
+      {children}
+      </Box>
+      <Box bottom="0" left="0" position="absolute">
+      Player
+      </Box>
+    </Box>
+  )
+}
+
+export default PlayerLayout;
